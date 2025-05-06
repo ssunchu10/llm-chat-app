92cfbb667826b1b032de9553ff9b4fb449aef0dc
Sumit <sumitsunchu@gmail.com>
2025-05-05 17:38:04 -0700

    Added Montserrat Font Globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ import type { Metadata } from "next";
 
 import { ThemeProvider } from "next-themes";
 import { Providers } from "./reducers/providers";
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+  display: 'swap',
+});
 
 import "./globals.css";
 
@@ -13,8 +20,8 @@ export const metadata: Metadata = {
 
 const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
-    <html suppressHydrationWarning lang="en">
-      <body className="bg-background text-foreground antialiased">
+    <html suppressHydrationWarning lang="en" className={montserrat.variable}>
+      <body className="bg-background text-foreground antialiased font-montserrat">
         <Providers>
           <ThemeProvider attribute="class">{children}</ThemeProvider>
         </Providers>
