0d23dd85e63768bf09238693586aac170b57b67f
Sumit <sumitsunchu@gmail.com>
2025-05-07 10:31:34 -0700

    Changed resetChat icon

--- app/components/Icons/NewChatIcon.tsx
+++ app/components/Icons/NewChatIcon.tsx
@@ -10,21 +10,21 @@ const NewChatIcon: React.FC<NewChatIconProps> = ({ onClick, className }) => {
     <button
       onClick={onClick}
       className={`p-2 rounded-md cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 transition ${className || ""}`}
-      title="New Chat"
+      title="Reset Chat"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        strokeWidth="2"
+        strokeWidth="1.91"
         strokeLinecap="round"
         strokeLinejoin="round"
-        className="w-8 h-8"
+        className="w-8 h-8 "
       >
-        <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
-        <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
-        <path d="M16 5l3 3" />
+        <path d="M18.68,1.48H5.32A3.82,3.82,0,0,0,1.5,5.3v9.54a3.82,3.82,0,0,0,3.82,3.82H9.14L12,21.52l2.86-2.86h3.82a3.82,3.82,0,0,0,3.82-3.82V5.3A3.82,3.82,0,0,0,18.68,1.48Z" />
+        <polyline points="12.08 9.99 9.3 7.21 12.08 4.42" />
+        <path d="M9.14,7.2H12A3.82,3.82,0,1,1,8.18,11" />
       </svg>
     </button>
   );
